d232d208439dcda9fe5614e5e89742560117443a
lenny_cheng <langrisser1981@gmail.com>
2022-12-24 11:30:03 +0800

    Initial Commit

---
+++ AutoOTP-TextField/AutoOTP_TextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoOTP_TextFieldApp.swift
+//  AutoOTP-TextField
+//
+//  Created by 程信傑 on 2022/12/24.
+//
+
+import SwiftUI
+
+@main
+struct AutoOTP_TextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
